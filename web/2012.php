ff5f82cd29d6da2fdbf2e4bb7918ad411196977c
Vincent Duchateau <34142072+v1ncee@users.noreply.github.com>
2017-12-04 10:21:26 +0100

    Update 2012.php

--- web/2012.php
+++ web/2012.php
@@ -61,7 +61,7 @@
     //Database <--> connection
     $link = mysqli_connect('db', 'user', 'test', "myDb");
 
-    $query = 'SELECT * From auto where jaar = 2012';
+    $query = 'SELECT * From Auto where jaar = 2012';
 
     $result = mysqli_query($link, $query);
 
